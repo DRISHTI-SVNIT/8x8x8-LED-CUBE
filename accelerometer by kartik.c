28bcc4b1961aeae95e7bafc106e6736574f389a4
Kartik Nighania <kkstrack@gmail.com>
2016-01-24 11:58:44 +0530

    IMU sensor values
    sensor readings on glove

---
+++ accelerometer by kartik.c
@@ -0,0 +1,12 @@
+/* below code will get values of IMU MPU6050 using I2C bus and will return a character as follows
+'F' forward
+'B' back
+'L' left
+'R' right
+'U' up
+'D' down
+using which further movement of led is possible by updating the 3D array (cube[x][y][z]) in main function 
+where x = floor
+	  y = flip-flop number
+	  z = led number
+*/ 
